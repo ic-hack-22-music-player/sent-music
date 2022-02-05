bacae5f4157dc6baebfd9cab366a6700b6afbd3f
Ezra Sitorus <es1419@ic.ac.uk>
2022-02-05 14:13:04 +0000

    ezra

--- frontend/music-player/src/App.js
+++ frontend/music-player/src/App.js
@@ -1,15 +1,25 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Audio from './components/Audio';
-import Header from './components/Header';
+import MIDISounds from 'midi-sounds-react';
 
-function App() {
-  return (
-    <div className="container">
-      <Header></Header>
-      <Audio />
-    </div>
-  );
+class App extends Component {
+  playTestInstrument() {
+    this.midiSounds.playChordNow(3, [60], 2.5);
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to midi-sounds-react example 1</h1>
+        </header>
+        <p className="App-intro">Press Play to play instrument sound.</p>
+        <p><button onClick={this.playTestInstrument.bind(this)}>Play</button></p>
+        <MIDISounds ref={(ref) => (this.midiSounds = ref)} appElementName="root" instruments={[3]} />
+      </div>
+    );
+  }
 }
 
 export default App;
