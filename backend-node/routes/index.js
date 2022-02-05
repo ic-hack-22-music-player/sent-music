a00b33a86b36af742c0a07d3803332071b39b5ef
Kitty Gu <kittygu@dyn3138-34.wlan.ic.ac.uk>
2022-02-05 16:24:59 +0000

    RNN in node

--- backend-node/routes/index.js
+++ backend-node/routes/index.js
@@ -1,12 +1,61 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+const music_rnn = require('@magenta/music/node/music_rnn');
+const core = require('@magenta/music/node/core');
+
+const model = new music_rnn.MusicRNN('https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/basic_rnn');
+model.initialize();
+
+const TWINKLE_TWINKLE = {
+    notes: [
+      {pitch: 60, startTime: 0.0, endTime: 0.5},
+      {pitch: 60, startTime: 0.5, endTime: 1.0},
+      {pitch: 67, startTime: 1.0, endTime: 1.5},
+      {pitch: 67, startTime: 1.5, endTime: 2.0},
+      {pitch: 69, startTime: 2.0, endTime: 2.5},
+      {pitch: 69, startTime: 2.5, endTime: 3.0},
+      {pitch: 67, startTime: 3.0, endTime: 4.0},
+      {pitch: 65, startTime: 4.0, endTime: 4.5},
+      {pitch: 65, startTime: 4.5, endTime: 5.0},
+      {pitch: 64, startTime: 5.0, endTime: 5.5},
+      {pitch: 64, startTime: 5.5, endTime: 6.0},
+      {pitch: 62, startTime: 6.0, endTime: 6.5},
+      {pitch: 62, startTime: 6.5, endTime: 7.0},
+      {pitch: 60, startTime: 7.0, endTime: 8.0},  
+    ],
+    totalTime: 8
+};
+
+router.get('/makeMusic', (req, res, next) => {
+    const qns = core.sequences.quantizeNoteSequence(TWINKLE_TWINKLE, 4);
+    model
+    .continueSequence(qns, 20, 1.5)
+    .then((sample) => {
+        let converted = core.sequenceProtoToMidi(sample)
+        console.log(converted);
+        let bytes = Buffer.alloc(converted.length);
+        for (let i = 0; i<bytes.length; i++) {
+            bytes[i] = converted[i];
+        }
+
+        fs.writeFile("test.mid", bytes, "binary", (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("YESSS");
+                res.send({res: "generated"});
+            }
+        })
+    });
+})
 
 router.get('/sendVoice', (req, res, next)=> {
 
 })
 
 router.post('/sendResult', (req, res, next) => {
-    
+
 })
 
 module.exports = router;
