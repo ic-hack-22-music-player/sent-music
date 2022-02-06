2058b77a9417f4f89ff630dc230f77d489a26d11
Kitty Baugh <kexinkittygu@gmail.com>
2022-02-06 09:44:49 +0000

    Save midi option

--- frontend/music-player/src/components/Back.js
+++ frontend/music-player/src/components/Back.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
-const Back = ({ onClick }) => {
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'  }} className='back'>
-      <h3 onClick={onClick} className="back-img">back</h3>
-    </div>
-  );
+const Back = ({onClick}) => {
+    const downloadMidi = () => {
+        let blob = new Blob([require('../generated.mid')], {type: 'application/midi'});
+
+        // let filePath = '../generated.mid';
+        let a = document.createElement('A');
+        a.href = window.URL.createObjectURL(blob);
+        a.download = 'generated.mid';
+        document.body.appendChild(a)
+        a.click()
+        document.body.removeChild(a)
+
+    }
+    return (
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}} className='back'>
+            <h3 onClick={onClick} className="back-img">back</h3>
+            <h3 onClick={downloadMidi} className="save-btn">save</h3>
+        </div>
+    );
 };
 
 export default Back;
